daee597f2aca9170637a1be35c1dd60fbcb3e490
mjtorrest <michaeljtt@outlook.com>
2023-06-28 14:32:59 -0500

    Encapsulamiento y Sobreescritura de la Clase Persona

--- src/Persona.java
+++ src/Persona.java
@@ -1,5 +1,5 @@
 public class Persona {
-    int id;
+    private int id;
     private String nombre;
     private String tel;
     private String email;
@@ -18,6 +18,53 @@ public class Persona {
         this.tel = tel;
         this.email = email;
     }
+
+    // GENERANDO GET Y SET
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    // FIN DE GET Y SET
+
+
+    @Override // // GENERANDO toString
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", tel='" + tel + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
 } // FIN CLASS PERSONA
 
 
