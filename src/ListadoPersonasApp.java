726d9a42533ddee1aa3f45fd839bcd62eabe8da9
mjtorrest <michaeljtt@outlook.com>
2023-06-28 15:59:07 -0500

    Creacion de los Casos Switch

--- src/ListadoPersonasApp.java
+++ src/ListadoPersonasApp.java
@@ -12,6 +12,12 @@ public class ListadoPersonasApp {
         var salir = false;
         while (!salir){
             mostrarMenu();
+            try {
+                salir = ejecutarOperacion(consola, personas);
+            } catch (Exception e){
+                System.out.println("Ocurrio un Error: " + e.getMessage());
+            }
+
             System.out.println();
         }
     }
@@ -25,6 +31,44 @@ public class ListadoPersonasApp {
                 3. Salir
                 """);
         System.out.print("Proporciona una Opcion: ");
+    }
+
+    private static boolean ejecutarOperacion(Scanner consola, List<Persona> personas) {
+        var opcion = Integer.parseInt(consola.nextLine());
+        var salir = false;
+        //REVISAR LA OPCION SELECCIONADA
+        switch (opcion){
+
+            case 1 -> { //AGREGAR PERSONA A LA LISTA -VARIABLE TEMPORAL
+                System.out.print("Proporcione el Nombre: ");
+                var nombre = consola.nextLine();
+                System.out.print("Proporcione el Telefono: ");
+                var tel = consola.nextLine();
+                System.out.print("Proporcione el Email: ");
+                var email = consola.nextLine();
+                // CREAR EL OBJETO PERSONA -VARIABLE TEMPORAL
+                var persona = new Persona(nombre, tel, email);
+                //LO AGREGAMOS A LA LISTA
+                personas.add(persona);
+                System.out.println("La Lista Tiene: " + personas.size() + " Elementos");
+            } // FIN DE CASO 1
+
+            case 2 -> { // LISTAR LAS PERSONAS
+                System.out.println("Listado de Personas: ");
+                // MEJORA USANDO LAMBDA Y METODO DE REFERENCIA
+                //personas.forEach((persona) -> System.out.println(persona)); <-- METODO LAMBDA
+                personas.forEach(System.out::println); // <-- METODO DE REFERENCIA
+            } // FIN DE CASO 2
+
+            case 3 -> { // SALIMOS DE CICLO
+                System.out.println("Hasta Pronto...");
+                salir = true;
+            } //FIN DE CASO 3
+
+            default -> System.out.println("Opcion Erronea " + opcion);
+        } // FIN DE SWITCH
+        return salir;
+
     }
 
 } // FIN DE CLASS
